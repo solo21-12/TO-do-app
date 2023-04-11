53d2df34561991e85accd19fc3fc45b98bf07317
solo2112 <dawitabrham0021@gmail.com>
2023-04-11 18:30:52 +0300

    update

--- containers/Footer/index.tsx
+++ containers/Footer/index.tsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
   return (
     <div >
-      <div className=" h-[1px] w-full bg-black"/>
-      <p className=" text-xs text-slate-600 font-serif text-center">Design and developed by Dawit Abraham</p>
+      <div className=" h-[1px] w-[80%]  mx-auto bg-black"/>
+      <p className=" text-xs text-slate-600 font-serif text-center mt-2">Design and developed by Dawit Abraham</p>
       <p className=" text-xs text-slate-600 font-serif text-center">@2023 Solo To Do</p>
     </div>
   );
